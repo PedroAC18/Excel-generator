630887089680d0a1b01aea135d7a35947d292bd7
PedroAC18 <pedro.correa1101@gmail.com>
2025-08-15 14:52:47 -0300

    adjusting project names and properties

--- src/main/java/com/beg/BankExtractExcelGeneratorApplication.java
+++ src/main/java/com/beg/BankExtractExcelGeneratorApplication.java
@@ -1,13 +1,13 @@
-package com.firstProject;
+package com.beg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class BankExtractExcelGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(BankExtractExcelGeneratorApplication.class, args);
 	}
 
 }
--- src/test/java/com/Application/BankExtractExcelGeneratorApplicationTests.java
+++ src/test/java/com/Application/BankExtractExcelGeneratorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.firstProject;
+package com.Application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class BankExtractExcelGeneratorApplicationTests {
 
 	@Test
 	void contextLoads() {
